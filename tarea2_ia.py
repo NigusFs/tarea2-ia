10c2cef6890a7cff40ebef1682acad29e1f916b5
NigusFs <nicolas.floress@mail.udp.cl>
2019-06-03 22:07:30 -0400

    final

--- tarea2_ia.py
+++ tarea2_ia.py
@@ -4,19 +4,23 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.metrics import pairwise_distances_argmin_min
 
-
+from sklearn.linear_model import LogisticRegression
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
-from mpl_toolkits.mplot3d import Axes3D
-plt.rcParams['figure.figsize'] = (16, 9)
-plt.style.use('ggplot')
+import os, psutil
+import time
 
-###parte 1
+from sklearn.metrics import classification_report, confusion_matrix  
+from mpl_toolkits.mplot3d import Axes3D
+
+start_time = time.time()
+
+
 
 def to_plot(X,name,color):
 
-	fig = plt.figure(name)
+	fig = plt.figure(name,figsize=(8, 6))
 	ax = Axes3D(fig)
 
 	ax.set_xlabel('Inhibidores destruidos por el equipo_1')
@@ -26,74 +30,72 @@ def to_plot(X,name,color):
 	ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=color,s=60) 
 	return ax
 
+def asignador_colores(etiquetas,colores):
+	aux=[]
+	for elem in etiquetas:
+		aux.append(colores[elem]) #le asigna un color a las etiquetas segun su valor
+	return aux
+
 
 
 #Dataset
 
 dataframe = pd.read_csv('games.csv') #https://www.kaggle.com/datasnaek/league-of-legends
-#print(dataframe.head())
-
 
 X = np.array(dataframe[["t1_inhibitorKills","t1_baronKills","t1_towerKills"]])
 y = np.array(dataframe['winner']) #arreglo de 1 y 2, el cual indica el gandor de la partida.
 
-
-colours=['black','blue','red'] #blue team 1, red team2
-aux=[]
-for fila in y:
-	aux.append(colours[fila]) #le asigna un color a la partida segun el ganador
-
-#to_plot(X,"Dataset",aux)
-
+plt.rcParams['figure.figsize'] = (24, 12)
+plt.style.use('ggplot')
+colours=[None,'blue','red'] #blue team 1, red team2
+colores_Dataset=asignador_colores(y,colours)
+to_plot(X,"Dataset",colores_Dataset)
 
 
 ###
 
 #K-Means
 
-k_means = KMeans(n_clusters=2).fit(X)
+k_means = KMeans(n_clusters=2, random_state=24).fit(X)
 centroides = k_means.cluster_centers_
-print(centroides)
+print("Ubicacion de los Centroides: \n",centroides,"\n")
 
 
-labels = k_means.predict(X)
+y_Kmeans = k_means.predict(X)+1
 C = k_means.cluster_centers_
-colours=['blue','red']
-aux=[]
 
-for fila in labels:
-    aux.append(colours[fila])
- 
 
-#ax = to_plot(X,"K-means",aux)
-#ax.scatter(C[:, 0], C[:, 1], C[:, 2], marker='*', c=colours, s=1000) #centroides
+colores_Kmeans=asignador_colores(y_Kmeans,colours)
+ax = to_plot(X,"K-means",colores_Kmeans)
+colours=['blue','red'] 
+ax.scatter(C[:, 0], C[:, 1], C[:, 2], marker='*', c=colours, s=1000) #centroides
+
+
 
 ###
 
-#Regresion lineal
+#Prediccion usando Regresion lineal
 
-regrL = linear_model.LinearRegression()
-regrL.fit(X,y)
+regrL = LogisticRegression(C=1.0, solver='lbfgs', multi_class='ovr')
+regrL.fit(X,y_Kmeans)
 
-#y_prediction = regrL.predict(X)
+x1 = 8 #t1_inhibitorKills #si es cero la prob que gane el t1 deberia ser baja
+y1 = 4 #t1_baronKills
+z1 = 11 #t1_towerKills # hay q elegir otros datos de la parttida, las variables del team 2 influyen mucho
 
-#print('Coefficients: \n', regrL.coef_)
-# Error cuadrático medio
-#print("Mean squared error: %.2f" % mean_squared_error(y, y_prediction))
-# Evaluamos el puntaje de varianza (siendo 1.0 el mejor posible)
-#print('Variance score: %.2f' % r2_score(y, y_prediction))
-
-x1 = 0#t1_inhibitorKills #si es cero la prob que gane el t1 deberia ser baja
-y1 =0 #t1_baronKills
-z1 =0#t1_towerKills #pesa harto, hay q elegir otros datos de la parttida, las variables del team 2 influyen mucho
 new_date =[x1,y1,z1]
-
 new_predict = regrL.predict([new_date]) # ("t1_inhibitorKills","t1_baronKills","t1_towerKills")
 
-print("Etiqueta del nuevo dato: ",int(new_predict))
-#print(y)
-#plt.show()
+print("Nuevos datos: ",new_date)
+print("[RegLogic] Etiqueta del nuevo dato: ",int(new_predict),"\n")
 
-#######
+## Pruebas para comparar los algoritmos 
+print("Eficiencia:")
+print ( "Tiempo de ejecucion: ", round(time.time() - start_time,3), "unidades de tiempo")
+process = psutil.Process(os.getpid())
+print( "Ram utilizada: ", round(process.memory_info().rss/1000000,2)," Mb\n")
 
-#parte 2
+print("Efectividad:")
+print(classification_report(y,y_Kmeans))  
+
+plt.show()
--- tarea2_ia_parte_2.py
+++ tarea2_ia_parte_2.py
@@ -1,26 +1,24 @@
-#https://stackabuse.com/k-nearest-neighbors-algorithm-in-python-and-scikit-learn/
-#https://www.datacamp.com/community/tutorials/k-nearest-neighbor-classification-scikit-learn
-#https://towardsdatascience.com/knn-using-scikit-learn-c6bed765be75
-
 import numpy as np  
 import matplotlib.pyplot as plt  
 import pandas as pd 
-
 from sklearn.model_selection import train_test_split
-
-
 from sklearn.preprocessing import StandardScaler  
 from sklearn.neighbors import KNeighborsClassifier  
-
 from sklearn.metrics import classification_report, confusion_matrix  
 from mpl_toolkits.mplot3d import Axes3D
-plt.rcParams['figure.figsize'] = (16, 9)
-plt.style.use('ggplot')
+import os, psutil
+import time
+
+from sklearn.metrics import classification_report, confusion_matrix
+
+start_time = time.time()
+
+
 
 
 def to_plot(X,name,color):
 
-	fig = plt.figure(name)
+	fig = plt.figure(name,figsize=(8, 6))
 	ax = Axes3D(fig)
 
 	ax.set_xlabel('Inhibidores destruidos por el equipo_1')
@@ -30,56 +28,63 @@ def to_plot(X,name,color):
 	ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=color,s=60) 
 	return
 
+def asignador_colores(etiquetas,colores):
+	aux=[]
+	for elem in etiquetas:
+		aux.append(colores[elem]) #le asigna un color a las etiquetas segun su valor
+	return aux
+
+
+## DataSet
 
 dataframe = pd.read_csv('games.csv') #https://www.kaggle.com/datasnaek/league-of-legends
-#print(dataframe.head())
 
 X = np.array(dataframe[["t1_inhibitorKills","t1_baronKills","t1_towerKills"]])
 y = np.array(dataframe['winner']) #arreglo de 1 y 2, el cual indica el gandor de la partida.
 
-colours=['black','blue','red'] #blue team 1, red team2
-aux=[]
-for fila in y:
-	aux.append(colours[fila]) #le asigna un color a la partida segun el ganador
 
-to_plot(X,"Dataset",aux)
+plt.rcParams['figure.figsize'] = (24, 12)
+plt.style.use('ggplot')
+colours=[None,'blue','red'] #blue = team 1, red= team2
+colores_Dataset=asignador_colores(y,colours)
+to_plot(X,"Dataset",colores_Dataset)
 
 
-x1 = 0 #t1_inhibitorKills #si es cero la prob que gane el t1 deberia ser baja
-y1 = 0 #t1_baronKills
-z1 = 0 #t1_towerKills #pesa harto, hay q elegir otros datos de la parttida, las variables del team 2 influyen mucho
-new_date =[x1,y1,z1]
+
+# KNN
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30,random_state=0) #Agrega ruido para evitar el overfiting
 
 knn =  KNeighborsClassifier(n_neighbors=4) 
-knn.fit(X,y)
-print(knn.predict([new_date])) #predicction work fine
+knn.fit(X_train,y_train)
+
+y_knn = knn.predict(X_test) #Agrupa los datos 
 
 
-#agregar ruido
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30) #creo q esta parte agrupa, intenta graficar esto
 
-colours=['black','blue','red']
-aux=[]
+colores_Knn=asignador_colores(y_knn,colours)
+to_plot(X_test,"Knn",colores_Knn)
 
-for fila in y_train:
-    aux.append(colours[fila])
- 
 
-to_plot(X_train,"Knn",aux) #i think this is not an agrupation of knn but who cares
+# Prediction
+x1 = 8 #t1_inhibitorKills #si es cero la prob que gane el t1 deberia ser baja
+y1 = 4 #t1_baronKills
+z1 = 11 #t1_towerKills #pesa harto, hay q elegir otros datos de la parttida, las variables del team 2 influyen mucho
+new_date =[x1,y1,z1]
+
+
+print("Nuevos datos: ",new_date)
+print("[Knn] La etiqueta del nuevo dato es :", knn.predict([new_date]),"\n") #predicction work fine 
+
+
+## Pruebas para comparar los algoritmos 
+
+print("Eficiencia:")
+print ( "Tiempo de ejecucion: ", round(time.time() - start_time,3), "unidades de tiempo")
+process = psutil.Process(os.getpid())
+print( "Ram utilizada: ", round(process.memory_info().rss/1000000,2)," Mb\n")
+
+print("Efectividad:") #depende de los datos de prueba y entrenamiento
+print(classification_report(y_test,y_knn))  
+
 plt.show()
-
-#agregar ruido
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
-#scaler = StandardScaler()   #ni p**o idea pa q funciona esto
-#scaler.fit(X_train)
-
-#X_train = scaler.transform(X_train)  
-#X_test = scaler.transform(X_test)  
-
-#classifier = KNeighborsClassifier(n_neighbors=4) 
-#classifier.fit(X_train, y_train) 
-#y_pred = classifier.predict(X_test) #prediccion, revisar que es X_test
-
-
-#print(confusion_matrix(y_test, y_pred))  
-#print(classification_report(y_test, y_pred))  
